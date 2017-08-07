7ddcc3cebbb5cdb562cf0e9f664780d97c9b6dd5
KathyLai <kathy.lai@senao.com.tw>
2017-08-07 15:27:57 +0800

    【課堂】Day 05 : 建立title component 單元/整合測試 (fake觀念)。

--- src/app/title/title.component.spec.ts
+++ src/app/title/title.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TitleComponent } from './title.component';
+import { By } from '@angular/platform-browser';
+
 
 describe('TitleComponent', () => {
   let component: TitleComponent;
@@ -21,5 +23,18 @@ describe('TitleComponent', () => {
 
   it('should be created', () => {
     expect(component).toBeTruthy();
+  });
+  describe(`Day 05 : <h1>todos</h1>`, () => {
+    it(`component中的title屬性為todos`, () => {
+      expect(component.title).toBe('todos');
+    });
+
+    it(`HTML裡面應該使用title屬性`, () => {
+      component.title = 'fake';
+      fixture.detectChanges();
+
+      const element = fixture.debugElement.query(By.css('h1')).nativeElement;
+      expect(element.textContent).toBe('fake');
+    });
   });
 });
